8bde87fd0d3ffaf9b65c76f8fdbf024ec9415b56
Jeff Lintel <jlintel@my365.bellevue.edu>
2019-11-30 20:16:35 -0600

    Added replies

---
+++ week-1/lintel-reply-to-kenney.js
@@ -0,0 +1,25 @@
+/*
+=================================================
+; Title: Assignment 1.1
+; Author: Sean Kenney - I did not reference any
+; other code other than what was in the text
+; Date: 27 November 2019
+; Modified by: Jeff Lintel
+; Description: I have corrected both errors. This 
+program now displays today's date and a past date.
+=================================================
+ 	*/
+ 	 
+ 	// variable that is assigned today's date
+ 	var todaysDate = new Date();
+ 	 
+ 	// variable that is assigned a past year
+ 	var pastYear = 1776;
+ 	 
+ 	// variable
+ 	 
+ 	//Display the current date
+ 	console.log("Today's date and time is " + todaysDate);
+ 	 
+ 	//Display a past year value
+ 	console.log(pastYear + " occurred in the past");
---
+++ week-1/lintel-reply-to-stacy.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Clayton Stacy
+; Date: 26 November 2019
+; Modified by: Jeff Lintel
+; Description: This program demonstrates the
+; use of JavaScript values and variables. It
+should print a date and the product of 2
+variables to the console.
+;===========================================
+*/
+
+//var newDate = new Date(13/1/2019).toLocaleDateString();
+var newDate = new Date("12/1/2019").toLocaleDateString();
+
+//var b = π;
+var b = Math.PI;
+
+var c = 50;
+console.log(newDate);
+
+console.log(b * c);
