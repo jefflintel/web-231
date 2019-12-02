88e81e13e84b597d42c7d2e7263cf6200d32f1d0
Jeff Lintel <jlintel@my365.bellevue.edu>
2019-12-02 14:04:51 -0600

    Added header and test

---
+++ week-2/lintel-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  lintel-header-test.js
+; Author: Professor Krasso
+; Date:   2 December 2019
+; Modified By: Jeff Lintel
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./lintel-header.js');
+
+console.log(header.display("Jeff", "Lintel", "Exercise 2.2"));
