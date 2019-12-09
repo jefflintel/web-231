52627992c0632657f9a4c2ccf4404361b30dad5a
Jeff Lintel <jlintel@my365.bellevue.edu>
2019-12-09 10:41:44 -0600

    Week 3 initial commit

---
+++ week-3/lintel-exercise-3.1.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  lintel-exercise-3.1.js
+; Author: Jeff Lintel
+; Date:   9 December 2019
+; Description: Demonstrate control statement
+; errors
+;===========================================
+*/
+
+const header = require('../lintel-header.js');
+
+console.log(header.display("Jeff", "Lintel", "Exercise 3.1"));
+
+//this loop should count down from 10 to 1, displaying each number
+ if [i = 10; i > 0; i--] {
+   console.log(i);
+ }
